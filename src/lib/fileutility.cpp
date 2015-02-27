90cf54cf78605683bb563b304c379b80324da3ce
rgaffu@gmail.com <rgaffu@gmail.com@40b2de02-c835-3440-6dca-cd0a4fdba288>
2015-02-27 17:29:29 +0000

    file utility

--- src/lib/fileutility.cpp
+++ src/lib/fileutility.cpp
@@ -5,7 +5,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/statvfs.h>
@@ -26,11 +25,24 @@ FileUtility::~FileUtility()
 }
 
 /////////////////////////////////////////////////////////////////////////////
+bool FileUtility::fileexist(const char *filename)
+{
+    FILE  *fdFile;
+    
+    if ((fdFile = fopen(filename, "r")) == NULL){
+        _VBL(1) << "file" << filename << " not exist";
+        return 0;
+    }
+    fclose(fdFile);
+    return true;
+}
+
+/////////////////////////////////////////////////////////////////////////////
 long int FileUtility::filesize(const char *filename)
 {
     FILE  *fdFile;
     
-	if((fdFile = fopen(filename, "r")) == NULL){
+	if ((fdFile = fopen(filename, "r")) == NULL){
 		_VBL(1) << "file" << filename << " not exist";
 		return 0;
 	}
@@ -43,6 +55,54 @@ long int FileUtility::filesize(const char *filename)
 }
 
 /////////////////////////////////////////////////////////////////////////////
+bool FileUtility::findstring(const char *filename, const char *str)
+{
+    bool ret = false;
+    
+    ifstream infile;
+    string line;
+    infile.open(filename);
+    
+    while (ret == false) {
+        getline(infile, line); 
+        if (infile.eof())
+            break;
+        
+        size_t found = line.find(str);
+        if (found != string::npos) {
+            _VBL(1) << "findstring: " <<  str << " in file " << filename;
+            ret = true;
+        }        
+    }
+    infile.close();
+    return ret;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+string FileUtility::line(const char *filename, const int numline)
+{
+    int num;
+    string line;
+    bool ret = false;
+    
+    ifstream infile;
+    infile.open(filename);
+    
+    num = 0;
+    line.clear();
+    while (ret == false) {
+        getline(infile, line); 
+        if (infile.eof())
+            break;
+        
+        if (num++ == numline) 
+            ret = true;
+    }
+    infile.close();
+    return line;
+}
+
+/////////////////////////////////////////////////////////////////////////////
 int FileUtility::domkdir(const char *directory)
 {
     typedef struct stat Stat;
@@ -51,13 +111,12 @@ int FileUtility::domkdir(const char *directory)
     int         mode = 0777;
 
     INF() << __func__ << directory;
-    if (stat(directory, &st) != 0)  {
+    if (stat(directory, &st) != 0) {
         /* Directory does not exist. EEXIST for race condition */
         if (mkdir(directory, mode) != 0 && errno != EEXIST)
             status = -1;
     }
-    else if (!S_ISDIR(st.st_mode))
-    {
+    else if (!S_ISDIR(st.st_mode)) {
         errno = ENOTDIR;
         status = -1;
     }
@@ -91,4 +150,3 @@ int FileUtility::check_free_space(const char *media)
         return -1;
     }    
 }
-
